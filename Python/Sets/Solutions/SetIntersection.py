3373719e4ab19804c99063a7af157834a27f22c9
kdbalabanov <k.d.balabanov@gmail.com>
2019-04-18 18:29:16 +0100

    Added solution for Python/Sets: SetIntersection

---
+++ Python/Sets/Solutions/SetIntersection.py
@@ -0,0 +1,6 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+n = int(input())
+N = set(input().split())
+m = int(input())
+M = set(input().split())
+print(len(N.intersection(M)))
