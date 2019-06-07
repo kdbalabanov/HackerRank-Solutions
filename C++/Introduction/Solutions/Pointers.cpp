72fd169eab3e51161e40cc1cc8306d2ff8d15f31
kdbalabanov <k.d.balabanov@gmail.com>
2019-06-07 21:21:53 +0100

    Added solution for C++/Introduction: Pointers

---
+++ C++/Introduction/Solutions/Pointers.cpp
@@ -0,0 +1,22 @@
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+void update(int *a, int *b) 
+{
+    *a = abs(*a + *b);
+    *b = abs(*a - *b - *b);    
+}
+
+int main() 
+{
+    int a, b;
+    int *pa = &a, *pb = &b;
+    
+    cin >> a >> b;
+    update(pa, pb);
+    cout << a << "\n" << b;
+
+    return 0;
+}
