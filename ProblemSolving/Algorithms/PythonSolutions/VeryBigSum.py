a6e68824b6b2a1584d4d53e408628178a2f69cb1
kdbalabanov <k.d.balabanov@gmail.com>
2019-06-18 20:30:30 +0100

    Added solution for ProblemSolving/Algorithms: VeryBigSum

---
+++ ProblemSolving/Algorithms/PythonSolutions/VeryBigSum.py
@@ -0,0 +1,12 @@
+#!/bin/python3
+
+import os
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    ar_count = int(input())
+    ar = list(map(int, input().rstrip().split()))
+    result = sum(ar)
+    fptr.write(str(result) + '\n')
+    fptr.close()
