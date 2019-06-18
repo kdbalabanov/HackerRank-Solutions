ed157accc32799049c07ee159e079e92ca5821ea
kdbalabanov <k.d.balabanov@gmail.com>
2019-06-18 20:44:24 +0100

    Small change for C++/Inheritance: MultiLevelInheritance

--- C++/Inheritance/Solutions/MultiLevelInheritance.cpp
+++ C++/Inheritance/Solutions/MultiLevelInheritance.cpp
@@ -7,20 +7,20 @@ using namespace std;
 
 class Triangle
 {
-	public:
-		void triangle()
+    public:
+        void triangle()
         {
-			cout << "I am a triangle" << endl;
-		}
+            cout << "I am a triangle" << endl;
+        }
 };
 
 class Isosceles : public Triangle
 {
-  	public:
-  		void isosceles()
+    public:
+        void isosceles()
         {
-    		cout << "I am an isosceles triangle" << endl;
-  		}
+            cout << "I am an isosceles triangle" << endl;
+        }
 };
 
 //Write your code here.
