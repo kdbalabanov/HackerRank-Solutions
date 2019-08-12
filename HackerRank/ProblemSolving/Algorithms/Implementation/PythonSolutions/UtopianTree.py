9ac9f8feb7de3dea3202299245984943cff13cee
kdbalabanov <k.d.balabanov@gmail.com>
2019-08-12 19:37:43 +0100

    Added solution for ProblemSolving/Algorithms/Implementation: UtopianTree

---
+++ HackerRank/ProblemSolving/Algorithms/Implementation/PythonSolutions/UtopianTree.py
@@ -0,0 +1,24 @@
+#!/bin/python3
+
+import os
+
+# Complete the utopianTree function below.
+def utopianTree(n):
+    result = 1
+    for i in range(0, n):
+        if i % 2 != 0:
+            result += 1
+        else:
+            result *= 2
+    return result
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    t = int(input())
+
+    for t_itr in range(t):
+        n = int(input())
+        result = utopianTree(n)
+        fptr.write(str(result) + '\n')
+    fptr.close()
