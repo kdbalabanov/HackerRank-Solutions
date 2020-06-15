12c0b7c2aad4cc0c4e79ed4842ea9ef101e41716
kdbalabanov <k.d.balabanov@gmail.com>
2020-06-15 21:58:11 +0100

    Added solution for LeetCode: TwoSum

---
+++ LeetCode/TwoSum.py
@@ -0,0 +1,15 @@
+# TwoSum problem: https://leetcode.com/problems/two-sum/
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        result = {}
+        
+        for i in range(0, len(nums)):
+            diff = target - nums[i]
+            
+            if diff in result:
+                return [result[diff], i]
+            
+            result[nums[i]] = i
+            
+        return result
