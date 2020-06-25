793d8da57e938ed55a1a75dc619c560d799ada95
kdbalabanov <k.d.balabanov@gmail.com>
2020-06-25 22:52:56 +0100

    Added solution for LeetCode: SingleNumber

---
+++ LeetCode/SingleNumber.py
@@ -0,0 +1,9 @@
+# Single Number problem: https://leetcode.com/problems/single-number/
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        
+        for i in range(1, len(nums)):
+            nums[0] ^= nums[i]
+    
+        return nums[0]
