ef71d7b01cbf84a1ef399adf2063ea34ce4d38a6
kdbalabanov <k.d.balabanov@gmail.com>
2020-12-13 16:53:54 +0000

    Added solution for LeetCode: RichestCustomerWealth

---
+++ LeetCode/RichestCustomerWealth.py
@@ -0,0 +1,5 @@
+# Richest customer wealth problem: https://leetcode.com/problems/richest-customer-wealth/
+
+class Solution:
+    def maximumWealth(self, accounts: List[List[int]]) -> int:
+        return max([sum(x) for x in accounts])
